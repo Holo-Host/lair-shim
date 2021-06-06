0d518aa2cc84e5213cfd383b79621d3d94b68fde
zo-el <joelulahanna@gmail.com>
2021-06-06 22:44:16 +0530

    update to create a cli fn

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
-// const { version } = require('../package.json')
-// const { Command } = require('commander')
+const { version } = require('../package.json')
+const { Command } = require('commander')
 const { init } = require('./shim')
 const path = require("path");
-const WH_SERVER_PORT = path.resolve(__dirname, '../shim/socket');
-const LAIR_SOCKET = path.resolve(__dirname, '../keystore/socket');
 
-init(LAIR_SOCKET, WH_SERVER_PORT, () => {
-  return null;
-});
+function main () {
+  const program = new Command()
+  program
+    .version(version)
+    .description(`CLI tool for spining up a shim for holochain.
+  example
+    lair-shim -t // to run in test mode
+  `)
+    .requiredOption('-p, --path <path>', 'assigns path to run lair-shim')
+    .requiredOption('-l, --lair_keystore_path <path>', 'path to running lair-keystore')
+    .option('-t, --test', 'Run lair-shim in test mode')
 
-// function main () {
-//   const program = new Command()
-//   program
-//     .version(version)
-//     .description(`CLI tool for spining up a shim for holochain.
-//   example
-//     lair-shim -t // to run in test mode
-//   `)
-//     .option('-t, --test', 'Run lair-shim in test mode')
-//     .option('-p, --path <path>', 'assigns path to run lair-shim', 4444)
-//
-//     program.parse(process.argv);
-//
-//     const options = program.opts();
-//     if (options.test) {
-//       console.log("Running Lair Shim in test mode...");
-//       init(LAIR_SOCKET, WH_SERVER_PORT, () => {
-//         return null;
-//       });
-//
-//     }
-//     else {
-//       console.log("Running Lair Shim...");
-//       init(LAIR_SOCKET, WH_SERVER_PORT, () => {
-//         return null;
-//       });
-//     }
-// }
-//
-// try {
-//   main()
-// } catch (e) {
-//   console.error(e.message)
-//   process.exit(1)
-// }
+    program.parse(process.argv);
+
+    const options = program.opts();
+
+    const SHIM_PATH = path.resolve(__dirname, options.path);
+    const LAIR_SOCKET = path.resolve(__dirname, options.lair_keystore_path);
+
+    if (options.test) {
+      console.log("Running Lair Shim in test mode...");
+      init(LAIR_SOCKET, SHIM_PATH, () => {
+        return null;
+      });
+    }
+    else {
+      console.log("Running Lair Shim...");
+      init(LAIR_SOCKET, SHIM_PATH, () => {
+        // TODO: This function needs to be uspader for production
+        return null;
+      });
+    }
+}
+
+try {
+  main()
+} catch (e) {
+  console.error(e.message)
+  process.exit(1)
+}
